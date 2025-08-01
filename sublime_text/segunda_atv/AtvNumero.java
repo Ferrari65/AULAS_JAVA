9b74f8740c783c9dcb9b6bd33edfc36a5877e8d2
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-08-01 15:40:04 -0300

    Exercicios para fixação

---
+++ sublime_text/segunda_atv/AtvNumero.java
@@ -0,0 +1,21 @@
+public class AtvNumero {
+	public static void main (String[] arg) {
+
+		int numero = 5;
+        numero =+ 3;
+
+        int numero2 = 5;
+        numero2 -= 2;
+
+        int numero3 = 4;
+        numero3 *= 4;
+
+        int numero4 = 4;
+        numero4 /= 2;
+
+		System.out.println(numero);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(numero4);
+	}
+}
