89e8fe4087c25511935e77a2c3508efbc94bd54d
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-08-01 12:36:48 -0300

    Operadores

---
+++ sublime_text/segunda_atv/OperadoresLogicos.java
@@ -0,0 +1,12 @@
+public class OperadoresLogicos {
+	public static void main (String[] args) {
+
+	int a = 10;
+	int b = 20;
+	boolean c = true;
+
+	System.out.println(a < b && c);
+	System.out.println(a > b || c);
+	System.out.println(!c);
+ }
+}
