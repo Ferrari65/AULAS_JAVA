361556f6d64e4d5c54841f27e7709f00ed3831c8
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-08-01 10:07:44 -0300

    Regras Java

---
+++ sublime_text/segunda_atv/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main (String[] args) {
+		byte a = 10;
+		int b = a + 5; // é promovido para Int antes do calculo
+		System.out.println("Valor de a = " + a );
+	}
+}
