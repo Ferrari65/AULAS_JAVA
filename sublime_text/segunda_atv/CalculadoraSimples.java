e1a7b8c43103bb9406362f29fbee159dcf10dcfa
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-08-01 15:28:57 -0300

    Exercicios para fixação

---
+++ sublime_text/segunda_atv/CalculadoraSimples.java
@@ -0,0 +1,13 @@
+public class CalculadoraSimples {
+	public static void main (String[] args) {
+		int a = 12;
+		int b = 45;
+
+		System.out.println( a + b );
+		System.out.println( a - b );
+		System.out.println( a * b );
+		System.out.println( a / b );
+		System.out.println( a % b );
+
+	}
+}
---
+++ sublime_text/segunda_atv/OperadoresUnarios.java
@@ -0,0 +1,8 @@
+public class OperadoresUnarios {
+	public static void main (String[] args) {
+		int a = 10;
+		a++; // Equivalente a (a = a + 1)
+
+		System.out.println(a);
+	}
+}
