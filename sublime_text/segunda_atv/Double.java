644110d080a0a4336ca5f24f55fba25bf93a4e67
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-08-01 10:26:39 -0300

    Promoções Automáticas

---
+++ sublime_text/segunda_atv/Double.java
@@ -0,0 +1,9 @@
+public class Double {
+	public static void main (String[] args) {
+		int a = 10;
+		double b = 1.5;
+		double c = a + b;
+
+		System.out.println("Valor de C = " + c);
+	}
+}
---
+++ sublime_text/segunda_atv/Float.java
@@ -0,0 +1,9 @@
+public class Float {
+	public static void main (String[] args) {
+		int a = 10;
+		float b = 1.5f;
+		float c = a + b; // a é promovido para Float
+
+		System.out.println("Valor de c = "+ c);
+	}
+}
