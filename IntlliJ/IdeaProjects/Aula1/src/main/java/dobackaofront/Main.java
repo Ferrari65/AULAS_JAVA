f3f8308c3820bb84f88f9cfa834040ddf52ddc2c
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-08-05 10:41:37 -0300

    Add files via upload

---
+++ IntlliJ/IdeaProjects/Aula1/src/main/java/dobackaofront/Main.java
@@ -0,0 +1,24 @@
+package dobackaofront;
+
+public class Main {
+    public static void main(String[] args) {
+        //modificadores de acesso -public -protected, private
+        /*
+         variaveis - tipos de dados (int float boolean char string)
+         estruturas condicionais - if - else - else if - switch
+         estrutura de repeticao - for - while do while
+         blocos de codigos (metodos)
+         orientacao a objetos POO
+         -classes
+         -objetos
+         -atriutos
+         -metodos
+         -construtores
+         - herancas
+         -polimorfismo
+         -associacao
+        * */
+
+        System.out.println("Hello World");
+    }
+}
